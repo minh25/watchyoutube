544ad6270c49a057cc199f30d0e9fa73eb9b617e
minh25 <minh00012258@gmail.com>
2021-07-17 14:43:12 +0700

    fadfadfasdf

--- maintenance.js
+++ maintenance.js
@@ -1,6 +1,6 @@
 const cmd = require("node-cmd");
 
-cmd.run("git pull --rebase", (err,data,stderr) => {
+cmd.run("git pull", (err,data,stderr) => {
     cmd.run("node yt.js");
     process.exit(1);
 });
